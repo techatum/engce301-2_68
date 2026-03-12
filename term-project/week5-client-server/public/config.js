84f53e2cf5ebf5571f3cc534b2aa45f971891035
techatum <potaeto99@gmail.com>
2026-03-12 11:42:07 +0700

    created term-pro week6

---
+++ term-project/week5-client-server/public/config.js
@@ -0,0 +1,9 @@
+// การตั้งค่า API
+const API_CONFIG = {
+    // เปลี่ยนเป็น VM IP ของคุณ
+    BASE_URL: 'http://192.168.56.101:3000',
+    ENDPOINTS: {
+        TASKS: '/api/tasks',
+        STATS: '/api/tasks/stats'
+    }
+};
